4ae7df2420d1ef79195b9814cf08b5d83791ca8f
panveel <swisshill60@gmail.com>
2019-01-21 13:47:12 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+
+
+
+   
+
+
+
+	// Mix It Up Activation
+	var portfolio_item = $('.portfolio-contant-active');
+	
+
+	$(".clients-logo").slick({
+		infinite: true,
+		arrows:false,
+		autoplay: true,
+  		autoplaySpeed: 2000
+	});
+
+	
+
+
+	
+
+
+});
+
+
+
+
